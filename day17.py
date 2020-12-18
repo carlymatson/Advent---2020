9ef789bb1ff473c01a213ef872059c87b42c956c
carlymatson <75649755+carlymatson@users.noreply.github.com>
2020-12-18 13:23:17 -0500

    Update day17.py
    I modified the original program to work flexibly for dimensions 2, 3, and 4.

--- day17.py
+++ day17.py
@@ -1,82 +1,101 @@
 inFile = open("day17input.text", 'r')
 
-# I've rotated the grid so that the array numbering coincides with the x and y axes.
-grid = inFile.readlines()
-xyzMins = [0,0,0]
-xyzMaxs = [len(grid), len(grid[0].strip()), 0]
+import operator
 
-# Add starting cubes to the active list.
-active = set()
-for i in range(xyzMaxs[0]):
-    s = grid[i].strip()
-    for j in range(xyzMaxs[1]):
-        if s[j] == '#':
-            p = tuple([i,j,0])
-            active.add(p)
+gridList = inFile.readlines()
 
-def countActive(_xyz):
+dim = 4 # Must be an integer greater than or equal to 2. Printing only works for dim=2,3,4.
+mins = [0]*dim
+maxs = [len(gridList), len(gridList[0].strip())] + [0]*(dim-2)
+
+# Returns a list of tuples representing the points of the grid.
+def getGrid(_mins, _maxs):
+    tupList = [[]]
+    for d in range(dim):
+        dTupList = []
+        for x in range(_mins[d], _maxs[d]+1):
+            tupListX = [tup + [x] for tup in tupList]
+            dTupList.extend(tupListX)
+        tupList = dTupList
+    return [tuple(tup) for tup in tupList]
+
+# Counts the active cubes adjacent to this one, excluding itself.
+def countActive(_cube):
     count = 0
-    for dx in range(-1,2):
-        for dy in range(-1,2):
-            for dz in range(-1,2):
-                dxyz = [dx, dy, dz]
-                p = tuple([_xyz[i]+dxyz[i] for i in range(3)])
-                if p in active and (dx!=0 or dy!=0 or dz!=0):
-                    count += 1
+    for delt in deltas:
+        if delt == tuple([0]*dim):
+            continue
+        p = tuple(map(operator.add, _cube, delt))
+        if p in active:
+            count += 1
     return count
 
-def expandBounds(_xyz):
-    for index in range(3):
-        if (_xyz[index] < xyzMins[index]):
-            xyzMins[index] = _xyz[index]
-        if (_xyz[index] > xyzMaxs[index]):
-            xyzMaxs[index] = _xyz[index]
+# Increases the size of the grid if needed.
+def expandBounds(_cube):
+    for index in range(dim):
+        if (_cube[index] < mins[index]):
+            mins[index] = _cube[index]
+        if (_cube[index] > maxs[index]):
+            maxs[index] = _cube[index]
+    grid = getGrid(mins, maxs)
     return True
 
-def printGrid():
-    print "----------------"
-    for z in range(xyzMins[2], xyzMaxs[2]+1):
-        print "--- z = %d ---"%(z)
-        for x in range(xyzMins[0], xyzMaxs[0]+1):
+def printGrid(): # This print function only works for dimensions 2, 3, and 4.
+    paneList = getGrid([0]*2+mins[2:], [0]*2+maxs[2:])
+    for pane in paneList:
+        if dim == 2:
+            print "---"
+        elif dim == 3:
+            print "--- z = %d ---"%(pane[2])
+        elif dim == 4:
+            print "--- z=%d, w=%d ---"%(pane[2],pane[3])
+        for x in range(mins[0], maxs[0]+1):
             charList = []
-            for y in range(xyzMins[1], xyzMaxs[1]+1):
-                cube = tuple([x,y,z])
+            for y in range(mins[1], maxs[1]+1):
+                cube = tuple([x,y]+list(pane[2:]))
                 if cube in active:
                     charList.append('#')
                 else:
                     charList.append('.')
+
             row = "".join(charList)
             print row
 
+
+
+
+## --------------- Main --------------- ##
+
+
+grid = getGrid(mins, maxs)
+deltas = getGrid([-1]*dim, [1]*dim)
+
+active = set()
+for i in range(maxs[0]):
+    s = gridList[i].strip()
+    for j in range(maxs[1]):
+        if s[j] == '#':
+            p = tuple([i,j]+[0]*(dim-2))
+            active.add(p)
+
 time = 0
-while time < 6:
+print "---------------- Cycle %d -----------------"%(time)
+printGrid()
+while time < 3:
     nowActive = set(active)
-    for x in range(xyzMins[0]-1, xyzMaxs[0] + 2):
-        for y in range(xyzMins[1]-1, xyzMaxs[1] + 2):
-            for z in range(xyzMins[2]-1, xyzMaxs[2] + 2):
-                thisCube = tuple([x,y,z])
-                num = countActive(thisCube)
-                #print "This cube: " + str(thisCube) + ", " + str(num)
-                if (thisCube in active) and (num != 2 and num != 3):
-                    #print "Become inactive"
-                    nowActive.remove(thisCube)
-                elif (thisCube not in active) and (num == 3):
-                    #print "Become active!"
-                    nowActive.add(thisCube) #Gotta expand bounds.
-                    expandBounds(thisCube)
-                    #print "New bounds?: " + str(xyzMins) + ", " + str(xyzMaxs)
-                else:
-                    #print "Do nothing"
-                    continue
+    for thisCube in getGrid([m-1 for m in mins], [M+1 for M in maxs]):
+        num = countActive(thisCube)
+        if (thisCube in active) and (num != 2 and num != 3):
+            nowActive.remove(thisCube)
+        elif (thisCube not in active) and (num == 3):
+            nowActive.add(thisCube)
+            expandBounds(thisCube)
+        else:
+            continue
     active = set(nowActive)
-    printGrid()
     time += 1
+    print "---------------- Cycle %d -----------------"%(time)
+    printGrid()
 
-#print active
 
-cube = tuple([1,1,1])
-print countActive(cube)
-
-print len(active)
-
-print sorted(active, key=lambda cube: cube[2])
+print "Number of active cubes: %d"%(len(active))
